76a09b9f4769b8c632853a6859b63d65bfdf864b
Arlan Uy <gnhs.com@gmail.com>
2022-02-09 15:02:13 -0600

    finish post functionality add a blog feature

--- my_own_frontend/spring-react-mongo/src/components/AddBlog.js
+++ my_own_frontend/spring-react-mongo/src/components/AddBlog.js
@@ -1,20 +1,47 @@
 import {Card, CardBody, Button, CardTitle, Row, Col, Input, Container} from 'reactstrap';
 import React from 'react';
+import {useState} from 'react';
+import axios from 'axios';
+import base_url from './../service/serviceapi';
+import { toast } from 'react-toastify';
 const AddBlog = () => {
+    const[blog, setBlog] = useState({});
+
+    const postData = (blog) => {
+        console.log("blog in postdata is " + JSON.stringify(blog));
+        axios.post(`${base_url}/create`, blog, {headers:{
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Methods': 'POST',
+             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Content-Type': 'application/json;charset=UTF-8'
+            }
+        }).then(response => {
+            toast.success("Created blog with " + blog.id + "successfully");
+        }).catch(error => {
+            if (error.message) {
+                toast.error("Server error : with message " + JSON.stringify(error.message));
+            }
+        })
+    } 
+
+    const handleForm= (param) => {
+        postData(blog)
+        param.preventDefault();
+    }
     console.log("passed here in addblog00");
     return (
         <Card>
             <CardBody>
-                <form onSubmit="#">
+                <form onSubmit={handleForm}>
                     <CardTitle className="display-2">
                         New Blog
                     </CardTitle>
-                    <Input placeholder="Enter the Blog Title here." onChange="#">
+                    <Input placeholder="Enter the Blog Title here." onChange={(e) => setBlog({...blog, title:e.target.value})}>
                         <br/>
                     </Input>
 
                     <Input type="textarea" rows="10"
-                    placeholder="Enter the Blog Content here." onChange="#">
+                    placeholder="Enter the Blog Content here." onChange={(e) => setBlog({...blog, content:e.target.value})}>
                         <br/>
                     </Input>
                     <Container>
